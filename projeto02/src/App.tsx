93bdefa7d94d8698de08d122f8e7c913d94b7017
rickdev8 <rique88858@gmail.com>
2025-04-24 22:34:01 -0300

    adc

---
+++ projeto02/src/App.tsx
@@ -0,0 +1,69 @@
+import "./App.css";
+import { FaGithub } from "react-icons/fa";
+import { CiSearch } from "react-icons/ci";
+import Card from "./components/card";
+import { useState } from "react";
+import { GetDados } from "./services/apirequest"
+import Loading from "./components/loading";
+import UnknowCardUser from "./components/unknowCardUser";
+
+function App() {
+  const [loading, setloading] = useState(false)
+  const [nome, setNome] = useState("");
+  const [dados, setDados] = useState<Record<string, string>>({});
+  const [erro, setErro] = useState(false);
+
+  const HandleSubmit = async () => {
+    setloading(true)
+    setDados({})
+    setTimeout( async () => {
+      try {
+        const response = await GetDados(nome);
+        setDados(response as unknown as Record<string, string>);
+        setErro(false)
+      } catch {
+        setErro(true)
+        setDados({});
+      } finally {
+        setloading(false)
+      }
+    }, 1000)
+  };
+  
+  return (
+    <div className="container-principal">
+      <div className="container-main">
+      <div className="title">
+        <FaGithub size={60} />
+        <h1>Perfil GitHub</h1>
+      </div>
+      <div className="input-search">
+        <input
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+          type="text"
+          placeholder="Digite o nome do usuário aqui"
+        />
+        <div className="input-icon">
+          <button onClick={HandleSubmit}>
+            <CiSearch size={35} color="white" />
+          </button>
+        </div>
+      </div>
+      {
+        loading && <Loading /> 
+      }
+
+      {
+        dados.name ? <Card link={dados.avatar_url} nome={dados.name} text={dados.bio}/> : null
+      }
+      
+      {
+        erro && <UnknowCardUser />
+      }
+    </div>
+    </div>
+  );
+}
+
+export default App;
